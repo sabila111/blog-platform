d27519c005c9e987d2a3e1dc590e3fe93a5c7b1a
sabila111 <sharminsharker2002@gmail.com>
2025-06-14 14:43:01 +0600

    first commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav>
+          <ul>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+          </ul>
+        </nav>
+    </div>
+  )
+}
